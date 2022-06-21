fa170f7eae33bea328f144ed25fd280633d3f084
eunjung <agjjung@naver.com>
2022-06-21 16:11:12 +0900

    프로젝트 패키지명 수정

--- app/src/main/java/com/itstime/haejo/mypage/MypageSetting.kt
+++ app/src/main/java/com/itstime/haejo/mypage/MypageSetting.kt
@@ -1,11 +1,12 @@
-package com.itstime.haejo.setting
+package com.itstime.haejo.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itstime.haejo.R
 
-class SettingMypage : AppCompatActivity() {
+class MypageSetting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_mypage)
+        setContentView(R.layout.activity_mypage_setting)
     }
 }
