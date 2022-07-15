74be8a66cafdf1bdb17a3c055b495fb149b9c4de
EunJung Jung <agjjung@naver.com>
2022-07-15 14:21:43 +0900

    fragment: chat, application 삭제, ongoing 추가

--- app/src/main/java/com/itstime/haejo/main/MainOngoingFragment.kt
+++ app/src/main/java/com/itstime/haejo/main/MainOngoingFragment.kt
@@ -5,30 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import com.itstime.haejo.R
-import com.itstime.haejo.databinding.FragmentMainChatBinding
-import com.itstime.haejo.databinding.FragmentMainHomeBinding
+import com.itstime.haejo.databinding.FragmentMainOngoingBinding
 
 
-class MainChatFragment : Fragment() {
+class MainOngoingFragment : Fragment() {
 
-    lateinit var binding : FragmentMainChatBinding
+    lateinit var binding : FragmentMainOngoingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentMainChatBinding.inflate(inflater, container, false)
+        binding = FragmentMainOngoingBinding.inflate(inflater, container, false)
+
+
 
         return binding.root
-
-
-
-
-
     }
 
 
